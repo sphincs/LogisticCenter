13f1375d8c0aef895a8424f56ed9886f9321cfa5
sphincs <sphincs@tut.by>
2016-08-05 10:20:13 +0300

    update data-script.sql

--- app-service/src/test/resources/data-script.sql
+++ app-service/src/test/resources/data-script.sql
@@ -1,7 +1,10 @@
 INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Mike', 35, '1 2', 5, '1111-ab1', 13.8);
 INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Bobby', 42, '1 2 3', 2, '2222-mn2', 7.5);
 INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Spencer', 50, '3 4', 10, '3333-zz8', 13.5);
-INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Misha', 36, '2', 2, '5555-yy7', 7.5);
+INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Misha', 36, '2', 2, '5555-yy7', 7.2);
+INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Rob', 36, '1 2', 0, '6666-yy7', 5.5);
+INSERT INTO DRIVERS (drivername, age, category, car, carnumber, fuelrate) VALUES ('Boris', 36, '2 4', 1, '6666-yy7', 6.8);
+
 
 INSERT INTO TRIPS (tripdriver, startpoint, endpoint, distance, startdate, enddate, sumfuel)
 VALUES (0,'brest', 'minsk', 350, '2016-06-30', '2016-06-30', 46.92);
